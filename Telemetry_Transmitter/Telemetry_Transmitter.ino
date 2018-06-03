7b994e11035b62fb716e2cb35304338ba28503c2
Donovan Paull <paulld@oregonstate.edu>
2018-06-03 16:25:09 -0700

    Button Press

--- Telemetry_Transmitter/Telemetry_Transmitter.ino
+++ Telemetry_Transmitter/Telemetry_Transmitter.ino
@@ -600,3 +600,15 @@ static void Data_Retrieve(){
       voltage = 2 * AnalogsensorValue * (3.3 / 1023.0);
     }
   }
+
+
+void Button_Press() {
+  static unsigned long last_interrupt_time = 0;
+    unsigned long interrupt_time = millis();
+    // If interrupts come faster than 200ms, assume it's a bounce and ignore
+    if(interrupt_time - last_interrupt_time > 300){
+      state = !state;
+      digitalWrite(ledPin, state);
+    }
+  last_interrupt_time = interrupt_time;
+  }
